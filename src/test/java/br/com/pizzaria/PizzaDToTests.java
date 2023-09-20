7d51b8a6d4ff5bb43498d4fdd1b001d5354454ae
regi <reginaldosgomesj@gmail.com>
2023-09-20 02:20:38 -0300

    Teste Unitários adicionados em algumas DTOS

---
+++ src/test/java/br/com/pizzaria/PizzaDToTests.java
@@ -0,0 +1,63 @@
+package br.com.pizzaria;
+
+import br.com.pizzaria.dto.PizzaDTO;
+import br.com.pizzaria.entity.Sabor;
+import br.com.pizzaria.entity.Tamanho;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+public class PizzaDToTests {
+
+    private List<Sabor> saborList = new ArrayList<>();
+    PizzaDTO pizza = new PizzaDTO(saborList,20,2, Tamanho.P);
+
+    @Test
+    void getsetNome(){
+        pizza.setQuantidade(3);
+        Assertions.assertEquals(3,pizza.getQuantidade());
+    }
+
+    @Test
+    void getsetNomes2(){
+        pizza.setPreco(20);
+        Assertions.assertEquals(20,pizza.getPreco());
+    }
+    @Test
+    void getsetNomes3(){
+        pizza.setTamanho(Tamanho.M);
+        Assertions.assertEquals(Tamanho.P,pizza.getTamanho());
+    }
+
+    @Test
+    void testeid(){
+        pizza.setId(2L);
+        Assertions.assertEquals(2,pizza.getId());
+    }
+
+    @Test
+    void testeComparacao(){
+        PizzaDTO pizza2 = new PizzaDTO(saborList,20,2,Tamanho.P);
+        Assertions.assertEquals(pizza,pizza2);
+    }
+
+    @Test
+    void testhashCode(){
+        PizzaDTO pizzaDTO1 = new PizzaDTO(saborList,21,2,Tamanho.P);
+        PizzaDTO pizzaDTO2 = new PizzaDTO(saborList,21,2,Tamanho.P);
+
+        Assertions.assertEquals(pizzaDTO1.hashCode(),pizzaDTO2.hashCode());
+    }
+
+    @Test
+    void testToString(){
+        PizzaDTO pizzaDTO2 = new PizzaDTO(saborList,20,2,Tamanho.P);
+        Assertions.assertEquals(pizzaDTO2.toString(),pizza.toString());
+
+    }
+
+}
---
+++ src/test/java/br/com/pizzaria/SaborDToTests.java
@@ -0,0 +1,47 @@
+package br.com.pizzaria;
+
+import br.com.pizzaria.dto.SaborDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SaborDToTests {
+
+    SaborDTO sabor = new SaborDTO("Bacon");
+
+
+    @Test
+    void getsetNome(){
+        sabor.setSabor("Queijo");
+        Assertions.assertEquals("Queijo",sabor.getSabor());
+    }
+
+    @Test
+    void testeid(){
+        sabor.setId(2L);
+        Assertions.assertEquals(2,sabor.getId());
+    }
+
+    @Test
+    void testeComparacao(){
+        SaborDTO sabor2 = new SaborDTO("Bacon");
+        Assertions.assertEquals(sabor,sabor2);
+    }
+
+    @Test
+    void testhashCode(){
+        SaborDTO saborDTO1 = new SaborDTO("Bacon");
+        SaborDTO saborDTO2 = new SaborDTO("Bacon");
+
+        Assertions.assertEquals(saborDTO1.hashCode(),saborDTO2.hashCode());
+    }
+
+    @Test
+    void testToString(){
+        SaborDTO saborDTO2 = new SaborDTO("Bacon");
+        Assertions.assertEquals(saborDTO2.toString(),sabor.toString());
+
+    }
+
+}
